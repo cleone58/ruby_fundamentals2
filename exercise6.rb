8f68e1d3bf1745fdda0f04b9f48caa2e11fe038c
Cleone Jacob <therealcleonejacob@gmail.com>
2016-01-13 18:29:59 -0500

    number of items

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,13 @@
  grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
  puts grocery_list
-
+grocery_list << "bread"
 
 grocery_list.each do |item|
    puts "* #{item}"
 
- end
+
+
+
+end
+
+puts grocery_list.length
